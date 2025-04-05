bec42dcb996c9edebb91d7a83abd38f3f4218f1e
AuraRizz <205314701+AuraRizz@users.noreply.github.com>
2025-04-05 19:24:57 +0200

    Generació
    Animació afegida perquè cada vegada que la velocitat en l'eix de les y sigui cap a dalt l'ocell vol. També un codi per fer que quan els murs surten de la camera del jugador es moguin a la dreta perquè no s'acabi mai el joc

--- Scripts/Moviment Murs.gd
+++ Scripts/Moviment Murs.gd
@@ -1,2 +1,15 @@
 extends Node2D
-#Hola
+var distancia_reutilitzar: float = 1500.0
+var distancia_entre_murs: float = 300.0
+@onready var mur = $"."
+@onready var flappy = $"../../CharacterBody2D"
+
+	
+func _process(delta: float) -> void:
+	moure()
+	
+func moure():
+	var distancia_fins_jugador = float(mur.position.x) - float(flappy.position.x)
+	if distancia_fins_jugador < -distancia_reutilitzar:
+		mur.position.x += 2610
+	
--- Scripts/flappy_bird.gd
+++ Scripts/flappy_bird.gd
@@ -3,15 +3,21 @@ extends CharacterBody2D
 var gravetat := Vector2.DOWN * 988
 var SALTS := -500
 var velocitat = 200
+@onready var animacio_jugador = $AnimatedSprite2D
 
 func _process(delta: float) -> void:
 	velocity.x = velocitat 
-	
+	animacio()
 	
 	
 	if Input.is_action_just_pressed("SALTAR"):
 		velocity.y = SALTS
 	velocity += gravetat * delta
 	move_and_slide()
-if velocity.y < 0:
-	$AnimatedSprite2D.play("Vola")
+	print(velocity.y)
+		
+func animacio():
+	if velocity.y < 0:
+		animacio_jugador.play("Volar")
+	else:
+		animacio_jugador.play("Normal")
