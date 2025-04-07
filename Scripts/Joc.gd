72fe878f4f3392cfddba6096841e9ed661d7bad9
AuraRizz <205314701+AuraRizz@users.noreply.github.com>
2025-04-07 16:35:19 +0200

    Desastre

--- Scripts/Joc.gd
+++ Scripts/Joc.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
-func _ready() -> void:
-	for Murs: Node2D in $Murs.get_children():
-		for detector: Area2D in 
-		detector.body_entered.connect(_on_detector_entered)
+#func _ready() -> void:
+#	for Murs: Area2D in $Murs.get_children():
+#		detector.body_entered.connect(_on_detector_entered)
 		
-func _on_detector_entered(body)->void:
-	pass
+
+@onready var flappy = $CharacterBody2D
+@onready var nous_murs = $Nous_Murs
+@export var escena_mur = preload("res://Escenes/Murs colisio.tscn")
+var x_posicio = 0
+var distancia_entre_murs = 300.
+var ultima_x = 0.
+
+func _ready():
+	ultima_x = $Mur.position.x
+	print(ultima_x)
+func _process(delta):
+	crea_mur(x_posicio)
+
+func crea_mur(x_posicio):
+	var mur_nou = escena_mur.instantiate()
+	nous_murs.add_child(mur_nou)
+	x_posicio = ultima_x + distancia_entre_murs 
--- Scripts/Moviment Murs.gd
+++ Scripts/Moviment Murs.gd
@@ -1,15 +1,7 @@
 extends Node2D
-var distancia_reutilitzar: float = 1500.0
-var distancia_entre_murs: float = 300.0
-@onready var mur = $"."
-@onready var flappy = $"../../CharacterBody2D"
 
-	
-func _process(delta: float) -> void:
-	moure()
-	
-func moure():
-	var distancia_fins_jugador = float(mur.position.x) - float(flappy.position.x)
-	if distancia_fins_jugador < -distancia_reutilitzar:
-		mur.position.x += 2610
-	
+var flappy: CharacterBody2D 
+
+func _process(delta):
+	if flappy and global_position.x < flappy.global_position.x - 800:
+		queue_free()
